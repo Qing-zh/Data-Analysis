97cec31ba74b3b49d4418d2282f252cb603f752a
Qing-zh <153347909+Qing-zh@users.noreply.github.com>
2024-07-05 10:34:48 +0800

    Add files via upload

--- 全球营养不良流行率趋势与地区差异分析.py
+++ 全球营养不良流行率趋势与地区差异分析.py
@@ -57,6 +57,33 @@ recent_year=data['Year'].max()
 recent_data=data[data['Year']==recent_year]
 print(recent_year)
 recent_data=pd.read_excel('D:/resource/数据分析/全球营养不良流行率趋势与地区差异分析/最新一年（2022年）各个地区的营养不良流行率.xlsx')
+
+global_data = data[data['Entity'] == 'World']
+fig = px.line(global_data, x='Year', y='2.1.1 Prevalence of undernourishment', title='Global Undernourishment Over Time')
+fig.update_traces(line_color='red')
+fig.update_layout(xaxis_title='Year', yaxis_title='Prevalence of Undernourishment (%)')
+fig.update_layout(plot_bgcolor='black', paper_bgcolor='black', font_color='red')
+fig.update_xaxes(showgrid=True, gridwidth=0.5, gridcolor='gray')
+fig.update_yaxes(showgrid=True, gridwidth=0.5, gridcolor='gray')
+fig.show()
+
+
+'''最新一年（2017年）各个地区的营养不良流行率'''
+# 创建条形图
+fig = go.Figure(data=go.Bar(x=recent_data['Entity'], y=recent_data['2.1.1 Prevalence of undernourishment'], marker_color='red'))
+# 更新布局
+fig.update_layout(title='Regional Undernourishment Comparison (Most Recent Year)',
+                  xaxis_title='Region',
+                  yaxis_title='Prevalence of Undernourishment (%)',
+                  xaxis_tickangle=-45,
+                  plot_bgcolor='black',
+                  paper_bgcolor='black',
+                  font=dict(color='red'),
+                  yaxis=dict(gridcolor='gray', gridwidth=0.5),
+                  showlegend=False)
+# 显示图表
+fig.show()
+
 #绘图1
 fig, ax = plt.subplots(figsize=(10, 6))
 ax.set_facecolor('black')
@@ -82,14 +109,29 @@ plt.xticks(rotation=45,color='red')
 plt.yticks(color='red')
 # plt.gca().spines['bottom'].set_color('red')
 # plt.gca().spines['left'].set_color('red')
-
 # 添加标题和标签
 plt.title('Regional Undernourishment Comparison(Most Recent Year) ', color='red')
 plt.xlabel('Region', color='red')
 plt.ylabel('Prevalence of Undernourishment(%)', color='red')
-
 # 显示图形
 plt.show()
+
+
+'''不同年份的营养不良流行率'''
+# 创建散点图
+fig = go.Figure(data=go.Scatter(x=data['Year'], y=data['2.1.1 Prevalence of undernourishment'], mode='markers', marker=dict(color='red')))
+# 更新布局
+fig.update_layout(title='Scatter Plot of Undernourishment vs Year',
+                  xaxis_title='Year',
+                  yaxis_title='Prevalence of Undernourishment (%)',
+                  plot_bgcolor='black',
+                  paper_bgcolor='black',
+                  font=dict(color='red'),
+                  yaxis=dict(gridcolor='gray', gridwidth=0.5),
+                  showlegend=False)
+
+# 显示图表
+fig.show()
 
 #绘制折线统计图
 import warnings
@@ -124,7 +166,6 @@ fig.update_layout(title='Global Undernourishment with Moving Average',
                   showlegend=True)
 # 显示图表
 fig.show()
-
 
 recent_data = data[data['Year'] == recent_year]
 recent_data = recent_data[recent_data['Entity'] != 'World']  # Exclude global total
